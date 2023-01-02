36f4764c5485009c7f56a5eeba6fe4ec9bacb6b1
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-01-02 12:22:59 +0600

    import my own picture

--- src/pages/register/AccountCreator.js
+++ src/pages/register/AccountCreator.js
@@ -18,22 +18,22 @@ const AccountCreator = () => {
     return <EmployerRegistration />;
   }
   return (
-    <div className="h-screen pt-14">
+    <div className=" pt-14">
       <h1 className="text-center my-10 text-2xl">Continue as ...</h1>
       <div className="flex justify-evenly ">
         <div
           onClick={() => navigate("/register/candidate")}
-          className="flex flex-col justify-between transition-all rounded-lg p-5 border border-white hover:border-purple-700 hover:shadow-2xl hover:scale-105 group cursor-pointer"
+          className="flex flex-col justify-between transition-all rounded-lg p-2 border border-white hover:border-purple-700 hover:shadow-2xl hover:scale-105 group cursor-pointer"
         >
           <img className="h-5/6" src={candidate} alt="" />
-          <p className="text-center text-3xl">Candidate</p>
+          <p className="text-center text-3xl mt-[-150px]">Candidate</p>
         </div>
         <div
           onClick={() => navigate("/register/employer")}
-          className="flex flex-col justify-between transition-all rounded-lg p-5 border border-white hover:border-purple-700 hover:shadow-2xl hover:scale-105 group cursor-pointer"
+          className="flex flex-col justify-between transition-all rounded-lg p-2 border border-white hover:border-purple-700 hover:shadow-2xl hover:scale-105 group cursor-pointer"
         >
           <img className="h-[77%]" src={employer} alt="" />
-          <p className="text-center text-3xl">Employer</p>
+          <p className="text-center text-3xl mt-[-150px]">Employer</p>
         </div>
       </div>
     </div>
